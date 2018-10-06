8a965f628047b6634794728971b65725d07fce6d
Ben Guzman <benguzman1010@gmail.com>
2018-10-06 16:53:14 -0500

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+		System.out.println("\n\tMy name is Ben.");
+		System.out.println("\tI am 17 years old.");
+	}
+	
+
+}
